9f55062098a6ab11751754b496013dcade521250
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-04-25 13:59:23 +0200

    operate

---
+++ src/logic/operate.js
@@ -0,0 +1,20 @@
+import BigJs from 'big.js'
+
+export default function operate(numberOne, numberTwo, operation) {
+  const numberOneBig = BigJs(parseFloat(numberOne))
+  const numberTwoBig = BigJs(parseFloat(numberTwo))
+
+  switch (operation) {
+    case '/':
+      return numberOneBig.div(numberTwoBig).valueOf()
+    case 'X':
+      return numberOneBig.times(numberTwoBig).valueOf()
+    case '-':
+      return numberOneBig.minus(numberTwoBig).valueOf()
+    case '+':
+      return numberOneBig.plus(numberTwoBig).valueOf()
+    default:
+      return new BigJs(0).valueOf();
+
+  }
+}
