da01bb69a6f9c7cd0afc62bc53062e0d09a14622
Ifeanyi Osinakayah Joseph <ifeanyi.Osinakayah@cousant.com>
2019-07-19 13:10:48 +0100

    Updates

---
+++ precache-manifest.7804d4ca641b18a4ee7a9f9dcf4308ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e0f43c2e8edef159da2",
+    "url": "/js_react_calculator/static/js/runtime~main.6e0f43c2.js"
+  },
+  {
+    "revision": "feb0e094b8cff4437cb3",
+    "url": "/js_react_calculator/static/js/main.feb0e094.chunk.js"
+  },
+  {
+    "revision": "cc37d7ed2e9703d8c808",
+    "url": "/js_react_calculator/static/js/1.cc37d7ed.chunk.js"
+  },
+  {
+    "revision": "feb0e094b8cff4437cb3",
+    "url": "/js_react_calculator/static/css/main.20a4eee4.chunk.css"
+  },
+  {
+    "revision": "2a31c7bb19743b9224707602fa5f7726",
+    "url": "/js_react_calculator/index.html"
+  }
+];
